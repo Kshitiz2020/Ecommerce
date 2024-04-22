b8b44741c7019330bedd8c7c5df0bd1abe575179
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-04-22 16:01:11 +0300

    click function is added on the based of content

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
+import { useState } from "react";
 
 function Navbar() {
+  const [menu, setMenu] = useState("kids");
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -10,12 +12,34 @@ function Navbar() {
         <p>SHOPPER</p>
       </div>
       <ul className="nav-menu">
-        <li>
-          Shop <hr />
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          Shop {menu === "shop" ? <hr /> : <></>}
         </li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li
+          onClick={() => {
+            setMenu("men");
+          }}
+        >
+          Men {menu === "men" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("women");
+          }}
+        >
+          Women {menu === "women" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          Kids {menu === "kids" ? <hr /> : <></>}
+        </li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
