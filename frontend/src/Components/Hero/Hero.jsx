7b49ca57a162240507a69980e267f12735ae2086
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-06-05 21:38:30 +0300

    new jsxhas been added

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -6,24 +6,28 @@ import hero_image from "../Assets/hero_image.png";
 
 function Hero() {
   return (
-    <div className="hero">
-      <div className="hero-left">
-        <h2>NEW ARRIVALS ONLY</h2>
-        <div>
-          <div className="hero-hand-icon">
-            <span>New</span>
-            <img src={hand_icon} alt="hand-icon" />
+    <div className="background-lg">
+      <div className="container">
+        <div className="hero">
+          <div className="hero-left">
+            <h2>NEW ARRIVALS ONLY</h2>
+            <div>
+              <div className="hero-hand-icon">
+                <span>New</span>
+                <img src={hand_icon} alt="hand-icon" />
+              </div>
+              <span>Collections</span>
+              <span> for Everyone</span>
+            </div>
+            <div className="hero-latest-btn">
+              <div>Latest Collection </div>
+              <img src={arrow_icon} alt="arrow_icon" />
+            </div>
           </div>
-          <span>Collections</span>
-          <span> for Everyone</span>
+          <div className="hero-right">
+            <img src={hero_image} alt="hero_image" />
+          </div>
         </div>
-        <div className="hero-latest-btn">
-          <div>Latest Collection </div>
-          <img src={arrow_icon} alt="arrow_icon" />
-        </div>
-      </div>
-      <div className="hero-right">
-        <img src={hero_image} alt="hero_image" />
       </div>
     </div>
   );
