34a8a29a61a15779714bdbd8b56768202bb0ccff
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-06-08 21:30:50 +0300

    context hooks being used

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Shop from "./Pages/Shop";
 import Cart from "./Pages/Cart";
 import Product from "./Pages/Product";
 import ShopCategory from "./Pages/ShopCategory";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
