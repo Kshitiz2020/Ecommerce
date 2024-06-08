5824831111a285653a61e56320d37aa53b72a106
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-06-08 17:48:48 +0300

    Offers components has been updated with the UI

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -5,8 +5,8 @@ function Item(props) {
       <img src={props.image} alt="image" />
       <p>{props.name}</p>
       <div className="item-prices">
-        <div className="item-price-new">{props.new_price}</div>
-        <div className="item-price-old">{props.old_price}</div>
+        <div className="item-price-new">${props.new_price}</div>
+        <div className="item-price-old">${props.old_price}</div>
       </div>
     </div>
   );
